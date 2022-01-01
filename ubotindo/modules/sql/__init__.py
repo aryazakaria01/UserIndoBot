e95afb5f8166096ff51ad0084ce13809ba13f6d2
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2022-01-01 16:00:19 +0700

    Update __init__.py

--- ubotindo/modules/sql/__init__.py
+++ ubotindo/modules/sql/__init__.py
@@ -14,19 +14,28 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import sys
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy.orm import scoped_session, sessionmaker
 
-from ubotindo import DB_URI
+from ubotindo import DB_URL, LOGGER
 
 
 def start() -> scoped_session:
-    engine = create_engine(DB_URI, client_encoding="utf8")
+    engine = create_engine(DB_URL, client_encoding="utf8")
+    LOGGER.info("PostgreSQL Connecting to database......")
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
 
 
 BASE = declarative_base()
-SESSION = start()
+try:
+    SESSION: scoped_session = start()
+except Exception as e:
+    LOGGER.exception(f"PostgreSQL Failed to connect due to {e}")
+    sys.exit()
+
+LOGGER.info("PostgreSQL Connection successful, session started.")
